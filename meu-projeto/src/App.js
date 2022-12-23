9b29be9a77e091559fa3e98f6dd4305ae057589b
Juarez Sant'Ana Vieira da Cruz <111532056+Juarezvieira@users.noreply.github.com>
2022-12-23 17:56:28 -0300

    Iniciei o curso e a prática.

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+
+  const name = "Juarez";
+
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
+  return (
+   <div className="App">
+    <h1>Olá React!</h1>
+    <p>Olá, {newName}</p>
+    <p>Soma: {sum (1, 2)}</p>
+    <img src={url} alt="Minha imagem"></img>
+   </div>
+  );
+}
+
+export default App;
