afcabec2cfc31cc2011280d29c016fc0d5ea621f
Juarez Sant'Ana Vieira da Cruz <111532056+Juarezvieira@users.noreply.github.com>
2022-12-26 20:18:07 -0300

    Fiz um exercício do curso.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import HelloWorld from './components/HelloWorld';
+
 function App() {
 
   const name = "Juarez";
@@ -17,7 +19,8 @@ function App() {
     <h1>Olá React!</h1>
     <p>Olá, {newName}</p>
     <p>Soma: {sum (1, 2)}</p>
-    <img src={url} alt="Minha imagem"></img>
+    <img src={url} alt="Minha imagem"/>
+    <HelloWorld />
    </div>
   );
 }
