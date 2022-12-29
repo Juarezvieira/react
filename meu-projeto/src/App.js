a17d11b7d5f7e9b9ea64d21270944333fd886804
Juarez Sant'Ana Vieira da Cruz <111532056+Juarezvieira@users.noreply.github.com>
2022-12-29 19:14:51 -0300

    Fiz os exercícios do curso.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,29 +1,16 @@
 import './App.css';
 
-import HelloWorld from './components/HelloWorld';
+import Evento from './components/Evento';
 
-import SayMyName from './components/SayMyName';
-
-import Pessoa from './components/Pessoa';
-
-import Frase from './components/Frase';
-
-import List from './components/List';
+import Form from './components/Form';
 
 function App() {
 
-  const nome = "Maria";
-
   return (
     <div className="App">
-       <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <HelloWorld />
-      <SayMyName nome="Matheus" />
-      <SayMyName nome={nome} />
-      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />
-      <List />
+       <h1>Testando Eventos</h1>
+       <Evento/>
+       <Form />
     </div>
   );
 }
